14e754afe1f35581e6563e760a171c7952dcc833
Maksim Shapaev <shapaev.maxim@gmail.com>
2024-12-15 17:25:57 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/style.css';
+
+
+import Character from "./js/character";
+import Bowman from "./js/bowman";
+import Swordsman from "./js/swordsman";
+import Magician from "./js/magician";
+import Daemon from "./js/daemon";
+import Undead from "./js/undead";
+import Zombie from "./js/zombie";
